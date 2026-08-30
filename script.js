56d6549eb4dfc0fbb478a6fb4ab1475bed47608f
Chukwuma Imeri <chuks4code@gmail.com>
2026-08-30 13:49:25 -0600

    added updated greeting

--- script.js
+++ script.js
@@ -24,6 +24,27 @@ function openModal(images, index) {
 }
 
 
+
+function updatedGreetings(){
+const hour = new Date().getHours();
+const greeting = document.getElementById("timeGreeting");
+
+     if (hour >= 5 && hour < 12) {
+        greeting.textContent = "Good Morning!";
+    } else if (hour >= 12 && hour < 17) {
+        greeting.textContent = "Good Afternoon!";
+    } else if (hour >= 17 && hour < 21) {
+        greeting.textContent = "Good Evening!";
+    } else {
+        greeting.textContent = "Good Night!";
+    }
+
+    
+}
+
+updateGreeting();
+
+
 /* ==========================
    UPDATE MODAL IMAGE
 ========================== */
